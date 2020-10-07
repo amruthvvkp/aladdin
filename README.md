c15b1fa16a577d2a5f0a25081cbd3ad3a523c096
Amruth VVKP <amruthvvkp@gmail.com>
2020-10-07 20:57:43 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Aladdin - Automated Learning and Autonomous Distributed Diversified Investment Network
+
+Aladdin is a model designed to trade using the user's Demat account just the way the 
+user does it. This is not a replacement of human effort but an assisted trading 
+to the actual human effort.
---
+++ setup.py
@@ -0,0 +1,16 @@
+"""Project Setup"""
+
+from setuptools import setup
+
+setup(
+    name='aladdin',
+    version='0.0.1',
+    packages=[''],
+    url='aas',
+    license='MIT',
+    author='Amruth VVKP',
+    author_email='amruthvvkp@gmail.com',
+    description='Aladdin - Automated Learning and Autonomous Distributed Diversified Investment Network - '
+                'is a model designed to trade using the user\'s Demat account just the way the user does it. '
+                'This is not a replacement of human effort but an assisted trading to the actual human effort .'
+)
