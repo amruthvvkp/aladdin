2f9c16c6dc32019d1ded2eed6fa9cab2d9987af3
Amruth Vvkp <amruthvvkp@gmailcom>
2021-12-28 18:22:11 +0000

    Fixes Update deepsource config file and yapf configuration for the project #26
    * Added a pyproject.toml config.

--- aladdin/prefect_example.py
+++ aladdin/prefect_example.py
@@ -1,18 +1,21 @@
 import time
 
-from prefect import task, flow
+from prefect import flow, task
 from prefect.task_runners import DaskTaskRunner
+
 
 @task
 def print_values(values):
     for value in values:
         time.sleep(0.5)
-        print(value, end="\r")
+        print(value, end='\r')
+
 
 @flow(task_runner=DaskTaskRunner())
 def parallel_flow():
-    print_values(["AAAA"] * 15)
-    print_values(["BBBB"] * 10)
+    print_values(['AAAA'] * 15)
+    print_values(['BBBB'] * 10)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     parallel_flow()
